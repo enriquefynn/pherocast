c94430c725282d536fb55d5ad4d7e0e9c770e7dd
Fynn <enriquefynn@gmail.com>
2015-06-17 22:42:32 -0300

    plots

--- scripts/graph_correct_trip.py
+++ scripts/graph_correct_trip.py
@@ -7,6 +7,8 @@ from math import hypot
 totalLine = 0
 nCorrects = 0
 trip = 1
+xmax = ymax = 0 
+xmin = ymin = float('inf')
 yLocPlot = []
 xLocPlot = []
 xCorPlot = []
@@ -15,7 +17,7 @@ yCorPlot = []
 for line in fileinput.input():
     totalLine+=1
     if line == '\n':
-        plt.plot(xCorPlot, yCorPlot, 'bo')
+        plt.plot(xCorPlot, yCorPlot, 'b.')
         plt.plot(xLocPlot, yLocPlot, 'r-')
         yLocPlot = []
         xLocPlot = []
@@ -27,8 +29,14 @@ for line in fileinput.input():
         xCorPlot+= [int(line[1])]
         yCorPlot+= [int(line[2])]
     if line[1] != '0' or line[2] != '0':
+        xmax = max(xmax, int(line[1]))
+        ymax = max(ymax, int(line[2]))
+        xmin = min(xmin, int(line[1]))
+        ymin = min(ymin, int(line[2]))
         xLocPlot+= [int(line[1])]
         yLocPlot+= [int(line[2])]
 
-plt.axis('equal')
-plt.show();
+print xmin, xmax, ymin, ymax
+plt.savefig('correctPredictions.png')
+plt.show()
+
